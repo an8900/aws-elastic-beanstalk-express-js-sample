f56d9c7d9290677f3e741d55694285dabf479b20
an8900 <87155536+an8900@users.noreply.github.com>
2021-07-09 11:23:50 +0900

    Update app.js
    FULL PIPELINE TEST

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('UOxOU ---Inu'));
+app.get('/', (req, res) => res.send('(・_・) >_<'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
